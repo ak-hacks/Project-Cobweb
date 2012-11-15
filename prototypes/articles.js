6f30b237969dfd2a14646f2bb992d183eeb7b012
Mark Stephens <mark.stephens@ft.com>
2012-11-15 15:40:00 +0000

    Frontend v1 and prototypes of visualisation

---
+++ prototypes/articles.js
@@ -0,0 +1,32 @@
+var articles = [
+  { 
+    "name": "FT article 1",
+    "type": "article",
+    "belongs": ["Financial Times", "Marjorie"]
+  },
+  { 
+    "name": "FT article 2",
+     "type": "article",
+   "belongs": ["Financial Times", "Marjorie"]
+  },
+  {
+    "name": "Google article 1",
+     "type": "article",
+   "belongs": ["Sergy Brin", "Google"]
+  },
+  {
+    "name": "Google article 2",
+     "type": "article",
+   "belongs": ["Larry Page", "Google"]
+  },
+  {
+    "name": "Google article 3",
+     "type": "article",
+   "belongs": ["Larry Page", "Google"]
+  },
+  {
+    "name": "Google article 4",
+     "type": "article",
+   "belongs": ["Larry Page", "Google"]
+  }
+]
---
+++ prototypes/items.js
@@ -0,0 +1,39 @@
+var items = [
+  {
+    "name": "Financial Times",
+    "type": "company",
+    "associations": [
+      "Marjorie"
+    ]
+  },
+  {
+    "name": "Google",
+    "type": "company",
+    "associations" : [
+      "Sergy Brin",
+      "Larry Page"
+    ]
+
+  },
+  {
+    "name": "Sergy Brin",
+    "type": "person",
+    "associations" : [
+    "Google"
+    ]
+  },
+  {
+    "name": "Marjorie",
+    "type": "person",
+    "associations" : [
+    "Financial Times"
+    ]    
+  },
+  {
+    "name": "Larry Page",
+    "type": "person",
+    "associations" : [
+      "Google"
+    ]
+  }
+  ]
